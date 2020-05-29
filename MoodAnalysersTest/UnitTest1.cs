910192bc822aecf0fff221f8047467c6d8d46c2c
unknown <vishalpwaman123@gmail.com>
2020-05-29 11:33:45 -0700

    Added: Code Of Given Mood when Sad should Return Sad

--- MoodAnalysersTest/UnitTest1.cs
+++ MoodAnalysersTest/UnitTest1.cs
@@ -10,12 +10,26 @@ namespace MoodAnalysersTest
         {
         }
 
+        /// <summary>
+        /// Added: Code Of Given Mood when Happy should Return Happy
+        /// </summary>
         [Test]
         public void givenMood_whenHappy_shouldReturnHappy()
         {
             moodAnalyser MoodA = new moodAnalyser("Happy");
             string result = MoodA.varificatioTest();
             Assert.AreEqual("Happy", result);
+        }
+
+        /// <summary>
+        ///  Added: Code Of Given Mood when Sad should Return Sad
+        /// </summary>
+        [Test]
+        public void givenMood_whenSad_shouldReturnSad()
+        {
+            moodAnalyser MoodA = new moodAnalyser("Sad");
+            string result = MoodA.varificatioTest();
+            Assert.AreEqual("Sad", result);
         }
 
     }
