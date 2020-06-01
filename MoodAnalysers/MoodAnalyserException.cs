32f0e3bae7ae5104d6c62b54e45bfa85874f7f7b
unknown <vishalpwaman123@gmail.com>
2020-05-31 21:26:39 -0700

    Added: Code Of Given Mood when Analysis Mood Create Object using Reflaction Pass Parametrized Constructor

--- MoodAnalysers/MoodAnalyserException.cs
+++ MoodAnalysers/MoodAnalyserException.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace MoodAnalysers
 {
-    class MoodAnalyserException
+    public class MoodAnalyserException : Exception
     {
+        public enum ExceptionType
+        {
+            ENTERED_NULL,
+            ENTERED_EMPTY,
+            No_Class_Exception,
+            No_Class_Method
+        }
+        public ExceptionType type;
+        public MoodAnalyserException(ExceptionType type, String message) : base(message)
+        {
+            this.type = type;
+        }
     }
 }
---
+++ MoodAnalysers/MoodAnalyserReflaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalysers
+{
+    public class MoodAnalyserReflaction
+    {
+        public static object CreateObjectUsingReflection(string className, params object[] construct)
+        {
+            Type type = Type.GetType(className);
+            if (type==null)
+            {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.No_Class_Exception, " ");
+            }
+            try
+            {
+                var objInstance = Activator.CreateInstance(type,construct);
+                if(objInstance==null)
+                {
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.No_Class_Method, " ");
+                }
+                return objInstance;
+            }catch(MissingMethodException)
+            {
+                return MoodAnalyserException.ExceptionType.No_Class_Method;
+            }
+             
+        }
+    }
+}
